921197328ed5893bce36624a1cfb08f044ba320a
Alexander Pataridze <s.pataridze@gmail.com>
2019-08-08 16:05:14 +0400

    Add tests. Update README

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,48 +1,83 @@
 /* eslint-env jest */
 /* global spyOn */
-import React, {useState} from 'react';
-import {cleanup, fireEvent, render} from 'react-testing-library';
-import {renderHook, act} from 'react-hooks-testing-library';
-import {Provider, useStore} from '../src';
+import React, { useState } from 'react';
+import { cleanup, fireEvent, render } from 'react-testing-library';
+import { renderHook, act } from 'react-hooks-testing-library';
+import { Provider, useStore } from '../src';
 
-const counterStore = () => {
-  const [count, setCount] = useState(0);
+const oddsStore = () => {
+  const [count, setCount] = useState(1);
 
-  const increment = (amount = 1) => setCount(count + amount);
-  const decrement = (amount = 1) => setCount(count - amount);
+  const increment = (amount = 2) => setCount(count + amount);
+  const decrement = (amount = 2) => setCount(count - amount);
 
-  return {count, setCount, increment, decrement};
+  return { count, setCount, increment, decrement };
 };
 
-const Counter = () => {
-  const {count, setCount, increment, decrement} = useStore(counterStore);
+const evensStore = () => {
+  const [count, setCount] = useState(0);
 
-  const updateState = () => setCount(100);
+  const increment = (amount = 2) => setCount(count + amount);
+  const decrement = (amount = 2) => setCount(count - amount);
+
+  return { count, setCount, increment, decrement };
+}
+
+const OddCounter = () => {
+  const { count, setCount, increment, decrement } = useStore(oddsStore);
+
+  const updateState = () => setCount(101);
+
+  console.log('Render');
 
   return (
     <div>
-      <span>Count: {count}</span>
-      <button onClick={() => decrement()}>-</button>
-      <button onClick={() => increment()}>+</button>
-      <button onClick={updateState}>set</button>
+      <span>Count Odd: <span data-testid='oddResult'>{count}</span></span>
+      <button data-testid='odd-' onClick={() => decrement()}>-</button>
+      <button data-testid='odd+' onClick={() => increment()}>+</button>
+      <button data-testid='oddSet' onClick={updateState}>set</button>
     </div>
   );
 };
 
-const brokenStore = () => {};
+const EvenCounter = () => {
+  const { count, setCount, increment, decrement } = useStore(evensStore);
+
+  const updateState = () => setCount(100);
+
+  console.log('Render');
+
+  return (
+    <div>
+      <span>Count Even: <span data-testid='evenResult'>{count}</span></span>
+      <button data-testid='even-' onClick={() => decrement()}>Even -</button>
+      <button data-testid='even+' onClick={() => increment()}>Even +</button>
+      <button data-testid='evenSet' onClick={updateState}>set</button>
+    </div>
+  );
+};
+
+const Counter = () => (
+  <>
+    <OddCounter />
+    <EvenCounter />
+  </>
+)
+
+const brokenStore = () => { };
 
 const BrokenCounter = () => {
-  const {state} = useStore(brokenStore);
+  const { state } = useStore(brokenStore);
   return <div>{state}</div>;
 };
 
 afterEach(cleanup);
 
-test('should incresase/decrease state counter in hook', () => {
+test('should increase/decrease state counter in hook', () => {
   let count, setCount;
-  renderHook(() => ({count, setCount} = counterStore()));
+  renderHook(() => ({ count, setCount } = oddsStore()));
 
-  expect(count).toBe(0);
+  expect(count).toBe(1);
 
   act(() => {
     setCount(1);
@@ -50,24 +85,53 @@ test('should incresase/decrease state counter in hook', () => {
   expect(count).toBe(1);
 });
 
-test('should incresase/decrease state counter in container', () => {
-  const {getByText} = render(
-    <Provider stores={[counterStore]}>
+test('should increase/decrease state counter in hook', () => {
+  let count, setCount;
+  renderHook(() => ({ count, setCount } = evensStore()));
+
+  expect(count).toBe(0);
+
+  act(() => {
+    setCount(2);
+  });
+  expect(count).toBe(2);
+});
+
+test('should increase/decrease state counter in container', () => {
+  const { getByTestId } = render(
+    <Provider stores={[oddsStore, evensStore]}>
       <Counter />
     </Provider>
   );
 
-  expect(getByText(/^Count:/).textContent).toBe('Count: 0');
+  spyOn(console, 'log');
 
-  fireEvent.click(getByText('+'));
-  fireEvent.click(getByText('+'));
-  expect(getByText(/^Count:/).textContent).toBe('Count: 2');
+  expect(getByTestId('oddResult').textContent).toBe('1');
+  expect(getByTestId('evenResult').textContent).toBe('0');
 
-  fireEvent.click(getByText('-'));
-  expect(getByText(/^Count:/).textContent).toBe('Count: 1');
+  fireEvent.click(getByTestId('odd+'));
+  fireEvent.click(getByTestId('odd+'));
+  expect(getByTestId('oddResult').textContent).toBe('5');
 
-  fireEvent.click(getByText('set'));
-  expect(getByText(/^Count:/).textContent).toBe('Count: 100');
+  fireEvent.click(getByTestId('even+'));
+  fireEvent.click(getByTestId('even+'));
+  expect(getByTestId('evenResult').textContent).toBe('4');
+
+  fireEvent.click(getByTestId('odd-'));
+  fireEvent.click(getByTestId('odd-'));
+  expect(getByTestId('oddResult').textContent).toBe('1');
+
+  fireEvent.click(getByTestId('even-'));
+  fireEvent.click(getByTestId('even-'));
+  expect(getByTestId('evenResult').textContent).toBe('0');
+
+  fireEvent.click(getByTestId('oddSet'));
+  expect(getByTestId('oddResult').textContent).toBe('101');
+
+  fireEvent.click(getByTestId('evenSet'));
+  expect(getByTestId('evenResult').textContent).toBe('100');
+
+  expect(console.log).toHaveBeenCalledTimes(10);
 });
 
 test('should throw error when no provider is given', () => {
